5e927541bc788f153560dbe8c941411f711de4f9
JustSharingSomeCode <62771039+JustSharingSomeCode@users.noreply.github.com>
2022-06-12 14:42:35 -0500

    Working scrollbar

---
+++ WpfCustomizableControls/Controls/WpfccScrollViewer.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfCustomizableControls.Controls
+{
+    /// <summary>
+    /// Realice los pasos 1a o 1b y luego 2 para usar este control personalizado en un archivo XAML.
+    ///
+    /// Paso 1a) Usar este control personalizado en un archivo XAML existente en el proyecto actual.
+    /// Agregue este atributo XmlNamespace al elemento raíz del archivo de marcado en el que 
+    /// se va a utilizar:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:WpfCustomizableControls.Controls"
+    ///
+    ///
+    /// Paso 1b) Usar este control personalizado en un archivo XAML existente en otro proyecto.
+    /// Agregue este atributo XmlNamespace al elemento raíz del archivo de marcado en el que 
+    /// se va a utilizar:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:WpfCustomizableControls.Controls;assembly=WpfCustomizableControls.Controls"
+    ///
+    /// Tendrá también que agregar una referencia de proyecto desde el proyecto en el que reside el archivo XAML
+    /// hasta este proyecto y recompilar para evitar errores de compilación:
+    ///
+    ///     Haga clic con el botón secundario del mouse en el proyecto de destino en el Explorador de soluciones y seleccione
+    ///     "Agregar referencia"->"Proyectos"->[Busque y seleccione este proyecto]
+    ///
+    ///
+    /// Paso 2)
+    /// Prosiga y utilice el control en el archivo XAML.
+    ///
+    ///     <MyNamespace:WpfccScrollViewer/>
+    ///
+    /// </summary>
+    public class WpfccScrollViewer : ScrollViewer
+    {
+        static WpfccScrollViewer()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(WpfccScrollViewer), new FrameworkPropertyMetadata(typeof(WpfccScrollViewer)));
+        }
+
+        #region Brushes
+
+        #region ScrollBarBackground
+
+
+        public Brush ScrollBarBackground
+        {
+            get { return (Brush)GetValue(ScrollBarBackgroundProperty); }
+            set { SetValue(ScrollBarBackgroundProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ScrollBarBackground.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ScrollBarBackgroundProperty =
+            DependencyProperty.Register("ScrollBarBackground", typeof(Brush), typeof(WpfccScrollViewer), new PropertyMetadata(new SolidColorBrush(Colors.DarkGray)));
+
+        #endregion
+
+        #region ThumbBrush        
+
+        public Brush ThumbBrush
+        {
+            get { return (Brush)GetValue(ThumbBrushProperty); }
+            set { SetValue(ThumbBrushProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ThumbBrush.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ThumbBrushProperty =
+            DependencyProperty.Register("ThumbBrush", typeof(Brush), typeof(WpfccScrollViewer), new PropertyMetadata(new SolidColorBrush(Colors.Gray)));
+
+        #endregion
+
+        #endregion
+
+        #region Border
+
+        #region CornerRadius
+
+
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CornerRadius.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.Register("CornerRadius", typeof(CornerRadius), typeof(WpfccScrollViewer), new PropertyMetadata(new CornerRadius(0)));
+
+        #endregion
+
+        #endregion
+
+        #region ContentMargin
+
+
+        public Thickness ContentMargin
+        {
+            get { return (Thickness)GetValue(ContentMarginProperty); }
+            set { SetValue(ContentMarginProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ContentMargin.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ContentMarginProperty =
+            DependencyProperty.Register("ContentMargin", typeof(Thickness), typeof(WpfccScrollViewer), new PropertyMetadata(new Thickness(5)));
+
+        #endregion
+    }
+}
